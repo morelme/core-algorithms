d6778cc833c3ae2a14b45ff11865a23f104dfd77
Melissa Morel <melmorel06@gmail.com>
2017-02-22 16:27:33 -0800

    refactored with ES6 and spacing consistency, also went from building unsorted array to initializing unsorted array

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -7,8 +7,8 @@ export default function mergeSort(arr) {
   }
 
   let middle = parseInt(arrLength/2)
-  var left = arr.slice(0, middle)
-  var right = arr.slice(middle, arrLength)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle, arrLength)
 
   return merge(mergeSort(left), mergeSort(right))
 }
@@ -16,7 +16,7 @@ export default function mergeSort(arr) {
 export function merge(left, right){
   let result = []
   while(left.length && right.length){
-    if (left[0]<= right[0]) {
+    if (left[0] <= right[0]) {
       result.push(left.shift())
     } else {
       result.push(right.shift())
--- test/mergeSort.js
+++ test/mergeSort.js
@@ -2,26 +2,13 @@ import { expect } from 'chai'
 import mergeSort from '../src/mergeSort'
 
 describe('mergeSort()', function(){
-  let arr
-  beforeEach(function() {
-    arr = new Array();
-    arr.push(34)
-    arr.push(203)
-    arr.push(3)
-    arr.push(746)
-    arr.push(200)
-    arr.push(984)
-    arr.push(198)
-    arr.push(764)
-    arr.push(9)
-  });
-
   it('should be a function', function(){
     expect(mergeSort).to.be.a('function')
   })
 
   it('sorts an array of numbers using the merge sort algorithm', function(){
+    const unsortedArray = [ 34, 203, 3, 746, 200, 984, 198, 764, 9]
     const sortedArr = [ 3, 9, 34, 198, 200, 203, 746, 764, 984 ]
-    expect(mergeSort(arr)).to.eql(sortedArr)
+    expect(mergeSort(unsortedArray)).to.eql(sortedArr)
   })
 })
