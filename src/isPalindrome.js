359543cf7dfaf32bb80bdd23c1f30cc3a300f858
Melissa Morel <melmorel06@gmail.com>
2017-02-28 17:25:11 -0800

    refactored algorithm to reduce time complexity

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,6 +1,11 @@
 export default function isPalidrome (str){
-  const regularExpression = /[^A-Za-z0-9]/g
-  const lowRegStr = str.toLowerCase().replace(regularExpression, '')
-  const reverseStr = lowRegStr.split('').reverse().join('')
-  return reverseStr === lowRegStr
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
 }
